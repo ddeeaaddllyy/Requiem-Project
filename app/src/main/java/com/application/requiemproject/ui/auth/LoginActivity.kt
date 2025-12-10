457cc7d68b809467e69e54cd0d48cf795d502b51
ddeeaaddllyy <naumova070783@gmail.com>
2025-12-10 23:15:16 +0300

    xd

---
+++ app/src/main/java/com/application/requiemproject/ui/auth/LoginActivity.kt
@@ -0,0 +1,40 @@
+package com.application.requiemproject.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.application.requiemproject.MainActivity
+import com.application.requiemproject.R
+import com.google.android.material.textfield.TextInputEditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val buttonLogin = findViewById<Button>(R.id.button_login)
+        val buttonRegister = findViewById<Button>(R.id.button_register)
+        val inputLogin = findViewById<TextInputEditText>(R.id.input_login)
+        val inputPassword = findViewById<TextInputEditText>(R.id.input_password)
+
+        buttonLogin.setOnClickListener {
+            val enteredEmail = inputLogin.text.toString().trim()
+            val enteredPassword = inputPassword.text.toString().trim()
+
+            if (true) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
