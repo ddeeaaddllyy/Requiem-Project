14daa9aa2c0aea19a61fe7d1bd863c3aaee0ecd5
ddeeaaddllyy <naumova070783@gmail.com>
2026-02-17 14:20:27 +0300

    add ability to create list of TextBlocks in suspend fun

--- app/src/main/java/com/application/requiemproject/data/repository/OCRRepository.kt
+++ app/src/main/java/com/application/requiemproject/data/repository/OCRRepository.kt
@@ -1,23 +1,36 @@
 package com.application.requiemproject.data.repository
 
 import android.graphics.Bitmap
+import com.application.requiemproject.model.TextBlock
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlinx.coroutines.tasks.await
 
 open class OCRRepository {
-    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    private val recognizer by lazy {
+        TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    }
 
-    suspend fun recognizeText(bitmap: Bitmap): String {
+    suspend fun recognizeText(bitmap: Bitmap): List<TextBlock> {
         return try {
             val inputImage = InputImage.fromBitmap(bitmap, 0)
-
             val result = recognizer.process(inputImage).await()
-            result.text
+            val detectedList = mutableListOf<TextBlock>()
+
+            for (block in result.textBlocks) {
+                detectedList.add(
+                    TextBlock(
+                        text = block.text,
+                        boundingBox = block.boundingBox
+                    )
+                )
+            }
+
+            detectedList
         } catch (e: Exception) {
             e.printStackTrace()
-            ""
+            emptyList()
         }
     }
 
