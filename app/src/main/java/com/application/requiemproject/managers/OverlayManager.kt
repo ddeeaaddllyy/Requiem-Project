39d3eaf67c13067e578022f75b9766230c5798c2
ddeeaaddllyy <naumova070783@gmail.com>
2026-02-17 14:24:23 +0300

    New OverlayManager.kt

---
+++ app/src/main/java/com/application/requiemproject/managers/OverlayManager.kt
@@ -0,0 +1,83 @@
+package com.application.requiemproject.managers
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.provider.Settings
+import android.util.Log
+import android.view.Gravity
+import android.view.WindowManager
+import android.widget.FrameLayout
+import com.application.requiemproject.model.TextBlock
+import com.application.requiemproject.view.TextOverlayView
+
+class OverlayManager(
+    private val context: Context
+)
+{
+    private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private var overlayView: FrameLayout? = null
+    private var drawView: TextOverlayView? = null
+
+    fun showOverlay() {
+        if (!Settings.canDrawOverlays(context)) {
+            Log.e("OverlayManager", "CRITICAL ERROR: application don't have 'Settings.canDrawOverlays' permission ")
+            return
+        }
+
+        if (overlayView != null) return
+
+        val layoutFlag = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            layoutFlag,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            PixelFormat.TRANSLUCENT
+        )
+        params.gravity = Gravity.TOP or Gravity.START
+
+        overlayView = FrameLayout(context).apply {
+            setBackgroundColor(Color.TRANSPARENT)
+        }
+
+        drawView = TextOverlayView(context).apply {
+            setBackgroundColor(Color.TRANSPARENT)
+        }
+
+        val drawParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        )
+        overlayView?.addView(drawView, drawParams)
+
+        try {
+            windowManager.addView(overlayView, params)
+            Log.d("OverlayManager", "Оверлей успешно добавлен на экран по: \nparams: ${params}\n overlayView:${overlayView}")
+        } catch (e: Exception) {
+            Log.e("OverlayManager", "Ошибка добавления оверлея: ${e.message}")
+            e.printStackTrace()
+        }
+    }
+
+    fun updateTextOnScreen(rects: List<TextBlock>) {
+        drawView?.setRect(rects)
+    }
+
+    fun removeOverlay() {
+        overlayView?.let {
+            try {
+                windowManager.removeView(it)
+            } catch (e: Exception) {
+                Log.e("OverlayManager", "Critical Error in removeOverlay(): $e")
+                e.printStackTrace()
+            }
+        }
+        overlayView = null
+        drawView = null
+    }
+}
