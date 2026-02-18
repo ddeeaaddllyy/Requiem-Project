499281db861989139d4c5eddf9e4bc24ab7cebbd
ddeeaaddllyy <naumova070783@gmail.com>
2026-02-18 14:28:04 +0300

    created final value. i don't know why but i had troubles with this val lol

--- app/src/main/java/com/application/requiemproject/notifications/NotificationActions.kt
+++ app/src/main/java/com/application/requiemproject/notifications/NotificationActions.kt
@@ -1,5 +1,5 @@
 package com.application.requiemproject.notifications
 
 object NotificationActions {
-    const val TOGGLE_CAPTURE = "action_toggle_capture"
+    final const val TOGGLE_CAPTURE = "action_toggle_capture"
 }
